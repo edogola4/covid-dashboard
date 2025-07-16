ab050c748d5d42f44877edf301619176192dc509
edogola4 <edogola4@gmail.com>
2025-07-16 15:30:12 +0300

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,vue,html,css,scss,less,ts,tsx,vue,html,css,scss,less}",
   ],
   theme: {
     extend: {
